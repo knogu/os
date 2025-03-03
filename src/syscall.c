e1f1a850d00f76fbb0b6db413ae9c29dff4de4ef
Kotaro Noguchi <kotaro.noguchi@linecorp.com>
2025-03-04 08:10:56 +0900

    putc

--- src/syscall.c
+++ src/syscall.c
@@ -4,20 +4,25 @@
 
 #define SYSCALL_INTR_NO	0x80
 
+enum SYSCALL_NO {
+    SYSCALL_PUTC,
+    MAX_SYSCALL_NUM
+};
+
 unsigned long long do_syscall_interrupt(
 	unsigned long long syscall_id, unsigned long long arg1,
-	unsigned long long arg2, unsigned long long arg3)
+	unsigned long long arg2 __attribute__((unused)), unsigned long long arg3 __attribute__((unused)))
 {
-	unsigned long long ret_val = 0xbeefcafe01234567;
-
-	puth(syscall_id, 16);
-	puts("\r\n");
-	puth(arg1, 16);
-	puts("\r\n");
-	puth(arg2, 16);
-	puts("\r\n");
-	puth(arg3, 16);
-	puts("\r\n");
+	unsigned long long ret_val = 0;
+    switch (syscall_id)
+    {
+        case SYSCALL_PUTC:
+            /* code */
+            putc((char)arg1);
+            break;
+        default:
+            break;
+    }
 
 	/* PICへ割り込み処理終了を通知(EOI) */
 	set_pic_eoi(SYSCALL_INTR_NO);
