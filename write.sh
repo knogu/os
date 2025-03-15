9a918b980b5b0f78e1d75c41239f3b5cd39065b3
knogu <kotaronoguchi61@gmail.com>
2025-03-15 09:37:53 +0900

    fix write.sh

--- write.sh
+++ write.sh
@@ -1,5 +1,6 @@
 cp ./fs/kernel.bin /Volumes/OS/
 cp ./fs/poiboot.conf /Volumes/OS/
+cp ./fs/fs.img /Volumes/OS/
 mkdir -p /Volumes/OS/EFI/BOOT
 cp ./fs/EFI/BOOT/BOOTX64.EFI /Volumes/OS/EFI/BOOT/
 diskutil unmount /Volumes/OS
